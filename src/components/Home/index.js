47a5171c34ebe400a8cd6781b942b400cc85e3f6
yamamotocamille <yamamotocamillejp@gmail.com>
2022-09-26 14:23:27 +0200

    home page css

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,16 @@
 // == Import
 import './styles.scss';
+import logo from 'src/assets/images/logo.png';
 /* eslint-disable react/button-has-type */
 
 // == Component
 function Home() {
   return (
     <div className="home">
-      <h1 className="home__title">O'Story Book</h1>
-      <button className="home__button">
+      <img className="home home__logo" src={logo} alt="logo" />
+      <button className="home home__button">
         Commencer l'aventure
       </button>
-
     </div>
   );
 }
