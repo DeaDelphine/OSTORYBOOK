bb24f753f7951f4bf75d9cead3b715a6ce0c9a6c
DeaDelphine <dealbadelphine@gmail.com>
2022-09-26 13:58:54 +0200

    add navlink route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,18 @@
 // == Import
+import { Routes, Route } from 'react-router-dom';
+
 import Header from '../Navigation/Header';
+import ConnectForm from '../ConnectForm';
+import Profil from '../Profil';
 import Home from '../Home';
 import Footer from '../Navigation/Footer';
+import ContactForm from '../Navigation/Footer/ContactForm';
+import LegalMentions from '../Navigation/Footer/LegalMentions';
+import GameRules from '../Navigation/Footer/GameRules';
+import Credits from '../Navigation/Footer/Credits';
 import './styles.scss';
+import MobileNavigation from '../Navigation/MobileNavigation';
+import CGU from '../Navigation/Footer/CGU';
 
 // == Composant
 function App() {
@@ -11,6 +21,18 @@ function App() {
       <Header />
       <Home />
       <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/connexion" element={<ConnectForm />} />
+        <Route path="/mon-compte" element={<Profil />} />
+        <Route path="/menu-mobile" element={<MobileNavigation />} />
+        <Route path="/cgu" element={<CGU />} />
+        <Route path="/nous-contacter" element={<ContactForm />} />
+        <Route path="/mention-legales" element={<LegalMentions />} />
+        <Route path="/credits" element={<Credits />} />
+        <Route path="/regle-du-jeu" element={<GameRules />} />
+      </Routes>
+
     </div>
   );
 }
