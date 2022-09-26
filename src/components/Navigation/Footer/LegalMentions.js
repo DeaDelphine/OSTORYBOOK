3a55d433557d63022274032ed36f0600482116af
DeaDelphine <dealbadelphine@gmail.com>
2022-09-26 17:43:12 +0200

    legacies adjust

--- src/components/Navigation/Footer/LegalMentions.js
+++ src/components/Navigation/Footer/LegalMentions.js
@@ -1,11 +1,31 @@
+/* eslint-disable max-len */
 // == Import
 import './styles.scss';
 
 // == Component
 function LegalMentions() {
   return (
-    <div className="legal-mentions">
-      <p>legal-mentions Component</p>
+    <div className="legal__mentions">
+      <h1 className="legal__mentions__title">Mentions Légales</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit perferendis ipsam,
+        nihil vero repudiandae amet temporibus, nostrum iste accusamus deleniti sed consequuntur debitis maxime dolorum deserunt suscipit inventore. Minima, labore.
+      </p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit perferendis ipsam,
+        nihil vero repudiandae amet temporibus, nostrum iste accusamus deleniti sed consequuntur debitis maxime dolorum deserunt suscipit inventore. Minima, labore.
+      </p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit perferendis ipsam,
+        nihil vero repudiandae amet temporibus, nostrum iste accusamus deleniti sed consequuntur debitis maxime dolorum deserunt suscipit inventore. Minima, labore.
+      </p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi cum temporibus nisi inventore nobis omnis corporis, at qui explicabo alias commodi ab architecto dolores? Molestiae eos voluptate atque pariatur praesentium!
+      Vero a maxime labore delectus consectetur dolor perferendis ullam odio eligendi nam cumque soluta quidem repellendus officia ab blanditiis magni, et, commodi, culpa voluptatem dicta fugiat at! Error, pariatur delectus.
+      Dignissimos, quaerat eius fugit ratione harum officia placeat dicta iste voluptatem commodi laborum vitae quas, perferendis amet deleniti eligendi pariatur at, voluptas nesciunt vel sit fugiat ullam nemo labore? Quibusdam.
+      Nesciunt odit deserunt obcaecati recusandae reprehenderit sapiente voluptate, corporis voluptatibus aliquam deleniti illo ipsam? Illo nesciunt quo obcaecati amet. Est blanditiis aliquam perferendis quam impedit, sequi similique? Necessitatibus, enim accusamus.
+      Aut, excepturi omnis. Harum inventore adipisci ad quo! Quia dolores ab exercitationem ad, ea modi dolore. Quibusdam voluptas fugiat repudiandae laudantium architecto voluptatum! Dicta aspernatur quae odio quo et saepe.
+      Assumenda, quod? Itaque laboriosam minus quo voluptatibus, quidem dolore nulla non repellat? Officia corrupti nam, numquam, ducimus omnis ullam, reprehenderit temporibus ipsam officiis impedit odio quo ex cupiditate totam beatae?
+      Excepturi debitis minima illo molestias cumque, repellendus accusamus commodi corrupti quod voluptatem, suscipit ex tenetur cum. Officia recusandae blanditiis accusamus fugit, dicta corporis minus vero culpa rem facilis quisquam debitis.
+      Nisi numquam esse ipsam. Adipisci reiciendis, sapiente accusantium in numquam sint explicabo corporis iste soluta, error, vero consectetur nulla expedita iusto! Iusto mollitia nobis libero voluptatum tempora velit neque unde!
+      Assumenda voluptate facilis ducimus enim, eius debitis sit omnis ab odio, ratione non natus totam vel. Autem commodi perspiciatis laboriosam doloribus sunt consectetur nesciunt rerum, nostrum repellat deleniti blanditiis deserunt!
+      Ipsum provident sit id! Eius, sapiente, repellat voluptatum, fuga nulla itaque nam autem ullam ea nisi error voluptas optio corporis nobis saepe perferendis accusamus. Provident temporibus obcaecati nisi repellendus mollitia!
     </div>
   );
 }
