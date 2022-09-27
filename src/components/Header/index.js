b67fbd64c07f7b3959d0ce31d1dd49dc0e5a6026
yamamotocamille <yamamotocamillejp@gmail.com>
2022-09-27 13:31:21 +0200

    Logo in Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@
 import { NavLink } from 'react-router-dom';
 import './styles.scss';
 import logo from 'src/assets/images/logo.png';
+import React, {useState} from 'react';
 /* eslint-disable react/button-has-type */
 
 // == Component
