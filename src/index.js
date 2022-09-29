0b7c5945df357e43cdce43ec4a95fa7a4ed878e4
DeaDelphine <dealbadelphine@gmail.com>
2022-09-29 08:24:18 +0200

    delete semantic ui

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { BrowserRouter } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
 
 // == Import : local
 // Composants
