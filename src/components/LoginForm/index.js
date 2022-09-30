0a643f35932b131c46a16b491eb912297d717b74
yamamotocamille <yamamotocamillejp@gmail.com>
2022-09-30 15:46:33 +0200

    LoginForm static integration

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-
 import Field from './Field';
 
 import './styles.scss';
@@ -11,69 +10,133 @@ import './styles.scss';
  *   - "not connected": displays the form and a button to connect
  */
 const LoginForm = ({
-  email,
-  password,
-  changeField,
-  handleLogin,
-  handleLogout,
-  isLogged,
-  loggedMessage,
-}) => {
+    email,
+    nickname,
+    password,
+    changeField,
+    handleLogin,
+    handleLogout,
+    isLogged,
+    loggedMessage,
+  }) => {
+    
   const handleSubmit = (evt) => {
     evt.preventDefault();
     handleLogin();
   };
 
   return (
-    <div className="container" >
+    <div className="container">
       <div className="login-form">
-        {isLogged && (
-          <div className="login-form-logged">
-            <p className="login-form-message">
-              {loggedMessage}
-            </p>
-            <button
-              type="button"
-              className="login-form-button"
-              onClick={handleLogout}
-            >
-              Déconnexion
-            </button>
+      <div className="login-form-container">
+        <div className="login-form-left">
+        <h2 className="login-form-left-title">INSCRIPTION</h2>
+        <p className="login-form-left-message">Pas encore de compte ? Inscrivez-vous pour pouvoir jouer.</p>
+          <div className="login-form-left-container">
+          {isLogged && (
+            <div className="login-form-logged">
+              <p className="login-form-message">
+                {loggedMessage}
+              </p>
+              <button
+                type="button"
+                className="login-form-button"
+                onClick={handleLogout}
+              >
+                Déconnexion
+              </button>
+            </div>
+          )}
+          {!isLogged && (
+            <form autoComplete="off" className="login-form-element" onSubmit={handleSubmit}>
+              <Field
+                name="email"
+                placeholder="Email"
+                onChange={changeField}
+                value={email}
+              />
+              <Field
+                name="nickname"
+                placeholder="Pseudonyme"
+                onChange={changeField}
+                value={email}
+              />
+              <Field
+                name="password"
+                type="password"
+                placeholder="Mot de passe"
+                onChange={changeField}
+                value={password}
+              />
+              <Field
+                name="password"
+                type="password"
+                placeholder="Confirmez le mot de passe"
+                onChange={changeField}
+                value={password}
+              />
+              <button
+                type="submit"
+                className="login-form-button"
+              >
+                S'INSCRIRE
+              </button>
+            </form>
+          )}
           </div>
-        )}
-        {!isLogged && (
-
-          <form autoComplete="off" className="login-form-element" onSubmit={handleSubmit}>
-            <Field
-              name="email"
-              placeholder="Adresse Email"
-              onChange={changeField}
-              value={email}
-            />
-            <Field
-              name="password"
-              type="password"
-              placeholder="Mot de passe"
-              onChange={changeField}
-              value={password}
-            />
-            <button
-              type="submit"
-              className="login-form-button"
-            >
-              OK
-            </button>
-          </form>
-        )}
+          </div>
+          <div className="login-form-right">
+          <h2 className="login-form-left-title">CONNEXION</h2>
+          <p className="login-form-left-message">Veuillez vous connecter pour jouer.</p>
+          <div className="login-form-right-container">
+          {isLogged && (
+            <div className="login-form-logged">
+              <p className="login-form-message">
+                {loggedMessage}
+              </p>
+              <button
+                type="button"
+                className="login-form-button"
+                onClick={handleLogout}
+              >
+                Déconnexion
+              </button>
+            </div>
+          )}
+          {!isLogged && (
+            <form autoComplete="off" className="login-form-element" onSubmit={handleSubmit}>
+              <Field
+                name="email-nickname"
+                placeholder="Email ou pseudonyme"
+                onChange={changeField}
+                value={email}
+              />
+              <Field
+                name="password"
+                type="password"
+                placeholder="Mot de passe"
+                onChange={changeField}
+                value={password}
+              />
+              <button
+                type="submit"
+                className="login-form-button"
+              >
+                SE CONNECTER
+              </button>
+            </form>
+          )}
+          </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-};
+  };
+
 
 LoginForm.propTypes = {
-  /** Contenu du champ e-mail */
   email: PropTypes.string.isRequired,
-  /** Contenu du champ password */
   password: PropTypes.string.isRequired,
   /**
    * Traitement déclenché quand on saisit un caractère dans l'un des deux champs.
