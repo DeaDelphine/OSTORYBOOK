5750a59f5937c76ddb6c3e46f438c14430155e82
yamamotocamille <yamamotocamillejp@gmail.com>
2022-10-03 14:16:23 +0200

    integration details

--- src/components/StoryList/Story/index.js
+++ src/components/StoryList/Story/index.js
@@ -1,41 +1,40 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable max-len */
 
+/* eslint-disable react/button-has-type */
+/* eslint-disable max-len */
 // == Import
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchPages } from 'src/actions/pages';
-
 import './styles.scss';
-
 // == Composant
 function Story({
   id, image, title, content, startPage,
 }) {
   const dispatch = useDispatch();
-
   return (
     <div className="story story-container" key={id}>
-      <img className="story story-container__img" src={image} alt="story-img" />
-      <h2 className="story story-container__title">{title}</h2>
-      <p className="story story-container__excerpt">{content}</p>
-
-      <Link to="/histoire">
-        <button
-          className="story story-container__button"
-          onClick={(event) => {
-            localStorage.setItem('id', id);
-            localStorage.setItem('startPage', startPage);
-            dispatch(fetchPages(id, startPage));
-          }}
-        >Commencer
-        </button>
-      </Link>
+      <div className="story story-container__img">
+        <img className="story story-container__img" src={image} alt="story-img" />
+        <h2 className="story story-container__title">{title}</h2>
+        <p className="story story-container__excerpt">{content}</p>
+        <Link to="/histoire">
+          <button
+            className="story story-container__button"
+            onClick={(event) => {
+              localStorage.setItem('id', id);
+              localStorage.setItem('startPage', startPage);
+              dispatch(fetchPages(id, startPage));
+            }}
+          >Commencer
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
-
 Story.propTypes = {
   id: PropTypes.number.isRequired,
   startPage: PropTypes.number.isRequired,
@@ -43,6 +42,5 @@ Story.propTypes = {
   content: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
 };
-
 // == Export
 export default Story;
