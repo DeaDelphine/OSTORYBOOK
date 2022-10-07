e4f2576819281961f4e0a83ea0b0374d382a1cba
yamamotocamille <yamamotocamillejp@gmail.com>
2022-10-07 16:51:13 +0200

    css details

---
+++ src/components/Footer/ContactForm.js
@@ -0,0 +1,15 @@
+// == Import
+import './styles.scss';
+
+// == Component
+function ContactForm() {
+  return (
+      <div className="contact-form">
+        <p>ContactForm Component</p>
+      </div>
+    </div>
+  );
+}
+
+// == Export
+export default ContactForm;
