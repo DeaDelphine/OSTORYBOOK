8838ed95636813d289f6965698c846dd5820b3c3
yamamotocamille <yamamotocamillejp@gmail.com>
2022-10-07 14:10:15 +0200

    contact form

---
+++ src/components/Footer/Contact/index.js
@@ -0,0 +1,15 @@
+// == Import
+import ContactForm from './ContactForm';
+import './styles.scss';
+
+// == Component
+function Contact() {
+  return (
+    <div className="container" >
+      <ContactForm />
+    </div>
+  );
+}
+
+// == Export
+export default Contact;
