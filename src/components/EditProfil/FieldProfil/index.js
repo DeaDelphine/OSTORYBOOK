a5148820d5d4bb6261a477cd9bedbd0a1557aac4
yamamotocamille <yamamotocamillejp@gmail.com>
2022-10-13 12:00:44 +0200

    Profil and ProfilEdit css modification, clean and comment

--- src/components/EditProfil/FieldProfil/index.js
+++ src/components/EditProfil/FieldProfil/index.js
@@ -10,16 +10,21 @@ function FieldProfil({
   const handleChange = (evt) => {
     onChange(evt.target.value, name);
   };
+
+  const inputId = `field-${name}`;
+
   return (
-    <input
-      className="field__input px-4 mb-2 h-12"
-      id={name}
-      type={type}
-      name={name}
-      placeholder={placeholder}
-      value={value}
-      onChange={handleChange}
-    />
+    <div className={value.length > 0 ? 'field field--has-content' : 'field'}>
+      <input
+        className="field-input"
+        id={name}
+        type={type}
+        name={inputId}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+      />
+    </div>
   );
 }
 
